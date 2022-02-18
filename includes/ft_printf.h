40afe64d86238a8c2e418a830fcf583d8d9345ff
Caruychen <caruychen@gmail.com>
2022-02-18 11:30:47 +0200

    consolidate parsers

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 18:03:47 by cchen             #+#    #+#             */
-/*   Updated: 2022/02/17 16:01:08 by cchen            ###   ########.fr       */
+/*   Updated: 2022/02/18 10:59:03 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 
 # include <stdarg.h>
 # include "vec.h"
+
+# define TRUE 1
+# define FALSE 0
 
 int	ft_printf(const char *format, ...);
 int	ft_vasprintf(char **ret, const char *format, va_list ap);
--- src/parser.c
+++ src/parser.c
@@ -6,11 +6,11 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 14:11:46 by cchen             #+#    #+#             */
-/*   Updated: 2022/02/18 10:32:39 by cchen            ###   ########.fr       */
+/*   Updated: 2022/02/18 11:30:26 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
+#include "ft_printf.h"
 #include "libft.h"
 
 int	parse_char(t_vec *result, int c)
@@ -37,12 +37,34 @@ int	parse_signed_int(t_vec *result, int n)
 	return (length);
 }
 
-int	parse_hex(t_vec *result, int n)
+int	parse_uint(t_vec *result, int n, int base, int uppercase)
 {
 	int		length;
 	char	*s;
 
-	s = ft_ultoa(n, 16);
+	s = ft_uitoa(n, base, uppercase);
+	length = parse_string(result, s);
+	free(s);
+	return (length);
+}
+
+int	parse_octal(t_vec *result, int n)
+{
+	int		length;
+	char	*s;
+
+	s = ft_uitoa(n, 8, 0);
+	length = parse_string(result, s);
+	free(s);
+	return (length);
+}
+
+int	parse_hex(t_vec *result, int n, int uppercase)
+{
+	int		length;
+	char	*s;
+
+	s = ft_uitoa(n, 16, uppercase);
 	length = parse_string(result, s);
 	free(s);
 	return (length);
@@ -56,17 +78,21 @@ int	parse_conversion(t_vec *result, const char **format, va_list ap)
 		parse_signed_int(result, va_arg(ap, int));
 	if (**format == 's')
 		parse_string(result, va_arg(ap, char *));
+	if (**format == 'o')
+		parse_uint(result, va_arg(ap, int), 8, FALSE);
+	if (**format == 'u')
+		parse_uint(result, va_arg(ap, int), 10, FALSE);
 	if (**format == 'x')
-		parse_hex(result, va_arg(ap, int)); 
+		parse_uint(result, va_arg(ap, int), 16, FALSE);
 	if (**format == 'X')
-		parse_hex(result, va_arg(ap, int)); 
+		parse_uint(result, va_arg(ap, int), 16, TRUE);
 	(*format)++;
 	return (result->len);
 }
 
 int	parse(t_vec *result, const char *format, va_list ap)
 {
-	const char *p;
+	const char	*p;
 
 	p = format;
 	while (*p)
