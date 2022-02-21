b2c4e894951338d2ba74e7a52900551715be29b5
Caruy Chen <caruychen@gmail.com>
2022-02-21 10:36:10 +0200

    copies va list

--- src/ft_vasprintf.c
+++ src/ft_vasprintf.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 13:39:23 by cchen             #+#    #+#             */
-/*   Updated: 2022/02/17 14:16:18 by cchen            ###   ########.fr       */
+/*   Updated: 2022/02/21 10:19:48 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@
 int	ft_vasprintf(char **ret, const char *format, va_list ap)
 {
 	t_vec	result;
+	va_list	ap_cpy;
 
 	if (vec_new(&result, 1, sizeof(char)) < 0)
 		return (-1);
+	va_copy(ap_cpy, ap);
 	if (parse(&result, format, ap) < 0)
 		return (-1);
 	*ret = result.memory;
+	va_end(ap_cpy);
 	return (result.len);
 }
