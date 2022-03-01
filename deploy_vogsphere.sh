aea90c862b8e6018b5fc22ee686ef8c710f9c2bf
Caruychen <caruychen@gmail.com>
2022-03-01 08:47:45 +0200

    fix deployment logic

--- deploy_vogsphere.sh
+++ deploy_vogsphere.sh
@@ -6,7 +6,7 @@
 #    By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/01 08:28:35 by cchen             #+#    #+#              #
-#    Updated: 2022/03/01 08:31:09 by cchen            ###   ########.fr        #
+#    Updated: 2022/03/01 08:47:35 by cchen            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,6 +15,6 @@ VOGSPHERE=$(ls | sed -n '/intra-uuid/p')
 rm -rf ./${VOGSPHERE}/*
 cp ./$REPO_NAME/Makefile ./${VOGSPHERE}
 cp ./$REPO_NAME/author ./${VOGSPHERE}
-cp -r ./$REPO_NAME/srcs ./${VOGSPHERE}
+cp -r ./$REPO_NAME/src ./${VOGSPHERE}
 cp -r ./$REPO_NAME/includes ./${VOGSPHERE}
 cp -r ./$REPO_NAME/libft ./${VOGSPHERE}
