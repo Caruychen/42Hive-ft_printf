05ac2aa5477dded5d7b0a7b786ea52f1afb4d5cb
Caruy Chen <caruychen@gmail.com>
2022-03-08 13:50:36 +0200

    handle integer width padding

--- src/convert_num.c
+++ src/convert_num.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 11:16:11 by cchen             #+#    #+#             */
-/*   Updated: 2022/03/08 13:11:29 by cchen            ###   ########.fr       */
+/*   Updated: 2022/03/08 13:50:15 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,35 @@ static inline int	is_null_precision(t_specs specs)
 {
 	return (specs.precision_on && specs.precision == 0 && specs.value == 0);
 }
-int	conv_num(t_vec *result, t_specs *specs)
+
+static int	push_result(t_vec *result, t_specs specs, char *s)
 {
 	int		res;
 	size_t	len;
+	
+	len = !is_null_precision(specs) * ft_strlen(s);
+	if (!(specs.flags & DASH))
+	{
+		padding(
+			result,
+			specs.width - (ft_imax(specs.precision, len)),
+			specs.pad_char);
+	}
+	padding(result, specs.precision - len, '0');
+	res = vec_append_strn(result, s, len);
+	if (specs.flags & DASH)
+	{
+		padding(
+			result,
+			specs.width - (ft_imax(specs.precision, len)),
+			specs.pad_char);
+	}
+	return (res);
+}
+
+int	conv_num(t_vec *result, t_specs *specs)
+{
+	int		res;
 	char	*s;
 	char	spec;
 
@@ -29,13 +54,7 @@ int	conv_num(t_vec *result, t_specs *specs)
 	s = g_modifier[specs->length](specs);
 	if (!s)
 		return (-1);
-	len = !is_null_precision(*specs) * ft_strlen(s);
-	padding(
-		result,
-		specs->width - (ft_imax(specs->precision, len)),
-		specs->pad_char);
-	padding(result, specs->precision - len, '0');
-	res = vec_append_strn(result, s, len);
+	res = push_result(result, *specs, s);
 	ft_strdel(&s);
 	return (res);
 }
