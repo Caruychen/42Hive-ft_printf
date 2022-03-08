05be0bcb912707d6db9d693d607b8ac7f7313fff
Caruy Chen <caruychen@gmail.com>
2022-03-08 10:21:43 +0200

    added width parsing

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 14:11:46 by cchen             #+#    #+#             */
-/*   Updated: 2022/03/08 09:54:48 by cchen            ###   ########.fr       */
+/*   Updated: 2022/03/08 10:07:50 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ int	parse(t_vec *result, const char *format, t_specs specs)
 			if (vec_append_strn(result, format, (p - 1) - format) < 0)
 				return (-1);
 			parse_flags(&p, &specs);
+			parse_width(&p, &specs);
 			parse_precision(&p, &specs);
 			parse_length(&p, &specs);
 			if (parse_conversion(result, &p, &specs) < 0)
