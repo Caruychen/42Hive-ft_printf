78c296bb551d2838cd0456678f8402b9cd638d37
Caruy Chen <caruychen@gmail.com>
2022-03-08 13:33:03 +0200

    handle width specifier with chars

--- src/convert_str.c
+++ src/convert_str.c
@@ -6,19 +6,30 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 11:13:57 by cchen             #+#    #+#             */
-/*   Updated: 2022/03/07 22:09:01 by cchen            ###   ########.fr       */
+/*   Updated: 2022/03/08 13:32:10 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "ft_printf.h"
 
+int	append_char(t_vec *result, t_specs *specs, char c)
+{
+	int	res;
+	if (!(specs->flags & DASH))
+		padding(result, specs->width - 1, specs->pad_char);
+	res = vec_push(result, &c);
+	if (specs->flags & DASH)
+		padding(result, specs->width - 1, specs->pad_char);
+	return (res);
+}
+
 int	conv_char(t_vec *result, t_specs *specs)
 {
 	char	c;
 
 	c = va_arg(specs->ap, int);
-	return (vec_append_strn(result, &c, 1));
+	return (append_char(result, specs, c));
 }
 
 int	conv_string(t_vec *result, t_specs *specs)
