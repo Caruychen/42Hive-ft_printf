3dcb90551fff62c89fc1265ac25286c275901e68
Caruy Chen <caruychen@gmail.com>
2022-03-09 11:42:38 +0200

    implement width spec in str

--- src/convert_str.c
+++ src/convert_str.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 11:13:57 by cchen             #+#    #+#             */
-/*   Updated: 2022/03/08 13:59:13 by cchen            ###   ########.fr       */
+/*   Updated: 2022/03/09 10:50:46 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,16 +33,28 @@ int	conv_char(t_vec *result, t_specs *specs)
 	return (append_char(result, specs, c));
 }
 
+static int	push_str(t_vec *result, t_specs specs, char *str)
+{
+	size_t	length;
+	int		res;
+
+	length = ft_strlen(str);
+	if (specs.precision_on && length > specs.precision)
+		length = specs.precision;
+	if (!(specs.flags & DASH))
+		padding(result, specs.width - length, specs.pad_char);
+	res = vec_append_strn(result, str, length);
+	if (specs.flags & DASH)
+		padding(result, specs.width - length, specs.pad_char);
+	return (res);
+}
+
 int	conv_string(t_vec *result, t_specs *specs)
 {
 	char	*str;
-	size_t	length;
 
 	str = va_arg(specs->ap, char *);
 	if (!str)
 		str = "(null)";
-	length = ft_strlen(str);
-	if (specs->precision_on && length > specs->precision)
-		length = specs->precision;
-	return (vec_append_strn(result, str, length));
+	return (push_str(result, *specs, str));
 }
