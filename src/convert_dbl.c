112b7e3ef48d924227e4c4eb4357b9f8633df407
Caruy Chen <caruychen@gmail.com>
2022-03-15 14:42:44 +0200

    more floats edge cases

--- src/convert_dbl.c
+++ src/convert_dbl.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 11:13:37 by cchen             #+#    #+#             */
-/*   Updated: 2022/03/15 14:18:10 by cchen            ###   ########.fr       */
+/*   Updated: 2022/03/15 14:42:32 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,19 +47,29 @@ static int	append_decimals(t_vec *vec, long double value,
 	return (vec->len);
 }
 
-static int	push_result(t_vec *result, t_vec *vec, t_specs specs)
+static int	mod_results(t_vec *result, t_vec *vec, t_specs specs)
 {
-	int		res;
 	char	*s;
 	size_t	offset;
 
 	s = mod_sign(specs);
 	offset = ft_strlen(s);
-	vec_append_strn(result, s, offset);
-	ft_strdel(&s);
+	if (specs.flags & ZERO && vec_append_strn(result, s, offset) < 0)
+		return (-1);
 	if (padding(result, specs.width - vec->len - offset, specs.pad_char,
 			!(specs.flags & DASH) && specs.width > vec->len))
 		return (-1);
+	if (!(specs.flags & ZERO) && vec_append_strn(result, s, offset) < 0)
+		return (-1);
+	ft_strdel(&s);
+	return (0);
+}
+
+static int	push_result(t_vec *result, t_vec *vec, t_specs specs)
+{
+	int		res;
+
+	mod_results(result, vec, specs);
 	res = vec_append(result, vec);
 	if (res < 0)
 	{
