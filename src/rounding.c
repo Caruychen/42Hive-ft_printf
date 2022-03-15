e9f95021628d5c559b90610309f74fd0c9fd1e72
Caruychen <caruychen@gmail.com>
2022-03-15 21:53:50 +0200

    alternative rounding

--- src/rounding.c
+++ src/rounding.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 16:14:48 by cchen             #+#    #+#             */
-/*   Updated: 2022/03/15 17:47:01 by cchen            ###   ########.fr       */
+/*   Updated: 2022/03/15 21:52:53 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,16 @@ long double	rounding(long double value, unsigned int precision)
 {
 	int				is_odd;
 	long double		pow;
+	long double		up;
+	long double		down;
 
 	pow = (long double) ft_pow(10, precision);
 	is_odd = (int) ft_fmod(value * pow, 2) % 2;
-	if (!is_odd && ft_fmod(value * pow, 1) - 0.5 == 0.0)
-		value -= 0.5 / pow;
-	return (value + 0.5 / pow);
+	if (!precision && !is_odd && ft_fmod(value * pow, 1) - 0.5 == 0.0)
+		return (value);
+	up = value + 0.5 / pow;
+	down = value - 0.5 / pow;
+	if ((up - value) > (value - down))
+		return (value);
+	return (up);
 }
