a0f6b7c55f59ecda4fd81e5d856a51eed7d51b12
Caruy Chen <caruychen@gmail.com>
2022-03-16 08:59:17 +0200

    move prototypes

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 18:03:47 by cchen             #+#    #+#             */
-/*   Updated: 2022/03/15 16:16:29 by cchen            ###   ########.fr       */
+/*   Updated: 2022/03/16 08:59:09 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,6 @@
 # define TRUE 1
 # define FALSE 0
 
-void		init_specs(t_specs *specs, va_list ap);
-void		reset_specs(t_specs *specs);
-int			get_int_base(const char c);
-int			padding(t_vec *result, int length, char c, int trigger);
-int			read_option(const char **format, t_specs *specs);
-long double	rounding(long double value, unsigned int precision);
-
 int			ft_printf(const char *format, ...);
 int			ft_vasprintf(char **ret, const char *format, va_list ap);
 int			parse(t_vec *result, const char *format, t_specs specs);
@@ -36,5 +29,11 @@ void		parse_length(const char **format, t_specs *specs);
 void		parse_precision(const char **format, t_specs *specs);
 void		parse_flags(const char **format, t_specs *specs);
 void		parse_width(const char **format, t_specs *specs);
+void		init_specs(t_specs *specs, va_list ap);
+void		reset_specs(t_specs *specs);
+int			get_int_base(const char c);
+int			padding(t_vec *result, int length, char c, int trigger);
+int			read_option(const char **format, t_specs *specs);
+long double	rounding(long double value, unsigned int precision);
 
 #endif
